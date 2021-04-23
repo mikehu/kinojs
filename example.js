1ee5b3de2c102a9d8e4dd2ac795f7a6d9e17f282
Mike Hu <mhu@neurox.net>
2021-04-22 20:45:22 -0700

    Update example.js

--- example.js
+++ example.js
@@ -30,100 +30,126 @@
 }());
 
 (function() {
-  const targetId = 'integration-block';
-  const targetEl = document.getElementById(targetId);
-  const afterTargetEl = targetEl.nextElementSibling;
+  const scenes = [];
+
+  let windowHeight;
+  const resetDimensions = function() {
+    windowHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+  }
+
+  const STICKY_STYLE = 'position: -webkit-sticky; position: sticky; width: 100%; top: 0';
+  const BEFORE_SCROLL_RANGE = 0;
+  const SCROLLING = 1;
+  const AFTER_SCROLL_RANGE = 2;
+
+  const Scene = function(options = {}) {
+    this._id = `scene-${scenes.length + 1}`;
+    this._targetEl = document.getElementById(options.target);
+    this._afterTargetEl = this._targetEl.nextElementSibling;
+    this._stickyContainer = document.createElement('div');
+    this._stickyContainer.id = this._id;
+    this._stickyContainer.style.width = '100%';
+    this._stickyFrame = document.createElement('div');
+    this._stickyFrame.style = STICKY_STYLE;
+    this._stickyFrame.setAttribute('data-scene-ref', this._id);
+    this._dimensions = {};
+    this._bounds = [];
+    this._durationPx = options.duration;
+    this._currState = null;
+    this._prevState = null;
+    this._listeners = [];
+    this.create()
+  }
+  Scene.prototype.create = function() {
+    const parentEl = this._targetEl.parentElement;
+    const refEl = this._afterTargetEl.nextElementSibling;
+    this._stickyFrame.appendChild(this._targetEl);
+    this._stickyFrame.appendChild(this._afterTargetEl);
+    this._stickyContainer.appendChild(this._stickyFrame);
+    parentEl.insertBefore(this._stickyContainer, refEl);
+    this.callibrate();
+    return this;
+  }
+  Scene.prototype.callibrate = function() {
+    const frameHeight = this._stickyFrame.clientHeight;
+    this._dimensions.targetElHeight = this._targetEl.clientHeight;
+    this._dimensions.targetScrollHeight = this._durationPx || frameHeight;
+    this._dimensions.scrollableHeight = this._dimensions.targetScrollHeight - frameHeight + this._dimensions.targetElHeight;
+    
+    const topBound = this._stickyFrame.offsetTop;
+    const bottomBound = topBound + this._dimensions.scrollableHeight - windowHeight;
+    this._bounds = [topBound, bottomBound]
+
+    window.requestAnimationFrame(() => {
+      this._stickyContainer.style.height = `${this._dimensions.targetScrollHeight}px`;
+    });
+    return this;
+  }
+  Scene.prototype.update = function(scrollPos) {
+    const changes = this._calculateChanges(scrollPos);
+
+    if (changes) {
+      this._applyChanges(changes);
+    }
+
+    if (this._currentState === SCROLLING) {
+      this._broadcast((scrollPos - this._bounds[0]) / (this._bounds[1] - this._bounds[0]));
+    }
+    return this;
+  }
+  Scene.prototype.tick = function(handlerFn) {
+    this._listeners.push(handlerFn);
+    return this;
+  }
+  Scene.prototype._calculateChanges = function(scrollPos) {
+    let changes = null;
+
+    this._prevState = this._currentState;
+    
+    if (scrollPos < this._bounds[0]) {
+      this._currentState = BEFORE_SCROLL_RANGE;
+    } else if (scrollPos < this._bounds[1]) {
+      this._currentState = SCROLLING;
+    } else {
+      this._currentState = AFTER_SCROLL_RANGE;
+    }
+
+    if (this._currentState !== this._prevState) {
+      changes = {
+        state: this._currentState
+        , direction: this._prevState === undefined ? 0 : this._currentState - this._prevState
+      };
+    }
+
+    return changes;
+  }
+  Scene.prototype._applyChanges = function(changes) {
+    this._stickyFrame.setAttribute('data-scene-state', changes.state);
+    if (changes.state === BEFORE_SCROLL_RANGE && changes.direction < 0) {
+      this._broadcast(0);
+    }
+    if (changes.state === AFTER_SCROLL_RANGE && changes.direction > 0) {
+      this._broadcast(1);
+    }
+  }
+  Scene.prototype._broadcast = function(progress) {
+    this._listeners.forEach((fn) => {
+      fn.call(this, progress);
+    });
+  }
+  Scene.prototype.destroy = function() {
+    if (!this._stickyContainer.parentElement) {
+      return;
+    }
+  }
 
   const frameImages = document.getElementById('frame-images');
   const frameTexts = document.getElementById('frame-texts');
-
-  const stickyContainer = document.createElement('div');
-  stickyContainer.style.width = '100%';
-  const stickyItem = document.createElement('div');
-  stickyItem.style = 'position: -webkit-sticky; position: sticky; width: 100%; top: 0';
-
-  const initElements = function() {
-    const parentEl = targetEl.parentElement;
-    const refEl = afterTargetEl.nextElementSibling;
-    stickyItem.appendChild(targetEl);
-    stickyItem.appendChild(afterTargetEl);
-    stickyContainer.appendChild(stickyItem);
-    parentEl.insertBefore(stickyContainer, refEl);
-  }
 
   const NUM_FRAMES = 3;
   const FRAMES_CLASSLIST = [];
   for (let i = 0; i < NUM_FRAMES; i++) {
     FRAMES_CLASSLIST.push(`show-frame-${i+1}`);
-  }
-  
-  let topBound;
-  let bottomBound;
-  let targetElHeight;
-  let scrollableHeight;
-  let targetScrollHeight;
-  let windowHeight;
-  let frameHeight = 1;
-
-  const determineBounds = function() {
-    windowHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-    targetElHeight = targetEl.clientHeight;
-    targetScrollHeight = targetElHeight * NUM_FRAMES;
-    scrollableHeight = targetScrollHeight - stickyItem.clientHeight + targetElHeight;
-    topBound = stickyItem.offsetTop;
-    bottomBound = topBound + scrollableHeight - windowHeight;
-    frameHeight = Math.floor(targetElHeight / NUM_FRAMES);
-
-    window.requestAnimationFrame(() => {
-      stickyContainer.style.height = `${targetScrollHeight}px`;
-    });
-  }
-
-  const NOT_YET_SCROLLING = 0;
-  const SCROLLING = 1;
-  const BEYOND_SCROLLING = 2;
-
-  let currentState, prevState;
-  const calculateStateChanges = function(y) {
-    let changed = null;
-
-    prevState = currentState;
-    
-    if (y < topBound) {
-      currentState = NOT_YET_SCROLLING;
-    } else if (y < bottomBound) {
-      currentState = SCROLLING;
-    } else {
-      currentState = BEYOND_SCROLLING;
-    }
-
-    if (currentState !== prevState) {
-      changed = {
-        state: currentState
-        , direction: prevState === undefined ? 0 : currentState - prevState
-        , y
-      };
-    }
-
-    return changed;
-  }
-
-  const applyChanges = function(changes) {
-    if (changes.state === NOT_YET_SCROLLING) {
-      stickyItem.setAttribute('data-scene-state', NOT_YET_SCROLLING);
-    }
-    if (changes.state === SCROLLING) {
-      stickyItem.setAttribute('data-scene-state', SCROLLING);
-      // if (changes.direction > 0) {
-      //   stickyItem.style.top = '0';
-      // }
-      // if (changes.direction < 0) {
-      //   stickyItem.style.top = `${windowHeight - targetElHeight}px`;
-      // }
-    }
-    if (changes.state === BEYOND_SCROLLING) {
-      stickyItem.setAttribute('data-scene-state', BEYOND_SCROLLING);
-    }
   }
 
   let currentFrame = 0;
@@ -140,37 +166,30 @@
 
   document.addEventListener('resize', () => {
     window.requestAnimationFrame(() => {
-      determineBounds();
+      resetDimensions();
+      scenes.forEach((scene) => {
+        scene.callibrate();
+      });
     });
   });
 
   document.addEventListener('scroll', () => {
     const y = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
-    const changes = calculateStateChanges(y);
+    scenes.forEach((scene) => {
+      scene.update(y);
+    });
+  });
 
-    if (changes) {
-      applyChanges(changes);
-    }
-
-    if (currentState === SCROLLING) {
+  resetDimensions();
+  scenes.push(new Scene({
+    target: 'integration-block'
+    , duration: 4000
+  }).tick((progress) => {
+    if (progress < 1) {
       window.requestAnimationFrame(() => {
-        const shouldLoadFrame = Math.min(Math.floor((y - topBound) / frameHeight) + 1, NUM_FRAMES);
+        const shouldLoadFrame = Math.floor(progress * NUM_FRAMES) + 1;
         loadFrame(shouldLoadFrame);
       });
     }
-  });
-
-  initElements();
-  determineBounds();
+  }));
 }());
-
-(function() {
-  const controller = new ScrollMagic.Controller();
-
-  new ScrollMagic.Scene({
-    duration: 1280
-    , offset: 20
-  })
-    .setPin('#integration-container')
-    .addTo(controller);
-}())
